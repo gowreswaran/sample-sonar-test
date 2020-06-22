80d07793fdcbe9b05379ab4839c32114e21f77da
Gowreswaran <venus.gowrish@gmail.com>
2020-06-22 11:50:40 +0530

    Test Sonarqube

--- src/main/java/com/kd/App.java
+++ src/main/java/com/kd/App.java
@@ -7,10 +7,15 @@ package com.kd;
 public class App {
 	public static void main(String[] args) {
 
-		if (true) {
-			m1();
+		boolean a = true;
+		if (a) {
+			m1(5);
+		} else {
+			m2();
+			m3(5);
 		}
 		m2();
+		m3(5);
 	}
 
 	private static String m2() {
@@ -25,12 +30,23 @@ public class App {
 		}
 	}
 
-	private static void m1() {
+	private static void m1(int a) {
 		System.out.println("Hello");
 	}
 
-	private static void m3() {
+	private static int m3(int a) {
 		System.out.println("Hello");
+
+		if (a == 5) {
+			return 25 * a;
+		}
+		if (a == 5) {
+			return 25 * a;
+		}
+		if (a == 10) {
+			return 25 * a;
+		}
+		return 75;
 	}
 
 }
